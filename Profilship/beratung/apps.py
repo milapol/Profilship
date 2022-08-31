01557a8a1dfc1292903466e00ab987b573848ad0
milapol <milapol@riseup.net>
2022-08-31 15:39:00 +0200

    Initial Commit

---
+++ Profilship/beratung/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BeratungConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'beratung'
---
+++ Profilship/infoundhilfe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InfoundhilfeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'infoundhilfe'
